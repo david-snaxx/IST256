2d866b030b5888619984d8c884ee383e744489e4
David <david.snaxx@gmail.com>
2026-03-29 14:22:42 -0400

    Updated dbServer to allow CORS and listen on port 3306 at localhost

--- db/dbServer.js
+++ db/dbServer.js
@@ -14,5 +14,20 @@ connection.connect(err => {
     console.log("Connected!");
 });
 
+app.use(express.json());
+// allow cross-origin requests
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Content-Type');
+    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    if (request.method === 'OPTIONS') {
+        return response.sendStatus(204);
+    }
+    next();
+})
+app.listen(3306, () => {
+    console.log("REST DB server running on http://localhost:3306");
+})
+
 const conferenceRoute = require("./routes/conferenceRoute.js");
-app.use("/conference", conferenceRoute(connection));
+app.use("/conferences", conferenceRoute(connection));
